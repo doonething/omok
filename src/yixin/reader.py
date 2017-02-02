99b1b289940a39168534719129419480acd7efa0
bi <a@a>
2017-02-02 13:07:32 +0900

    making supervised learning inputs and targets from from yin save files

---
+++ src/yixin/reader.py
@@ -0,0 +1,109 @@
+'''
+Created on 2017. 2. 2.
+
+@author: bi
+'''
+
+import copy
+
+class YixinSave :
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+        self.num = 0
+        self.blacks = []
+        self.whites = []
+
+    def read_white_win(self, path):
+        self.read_yixin(path)
+        self.make_features()
+        return self.make_input_targets_for_white_win()
+        
+    def read_black_win(self, path):
+        self.read_yixin(path)
+        self.make_features()
+        return self.make_input_targets_for_black_win()
+            
+    def read_yixin(self, path):
+        with open (path) as f :
+            lines = f.readlines()
+            self.width  = int ( lines[0])
+            self.height = int ( lines[1])
+            self.num    = int ( lines[2])
+            current = self.blacks
+            for line in lines [3:]:
+                splited = line.split(' ')
+                y_index = int ( splited[0])
+                x_index = int ( splited[1])
+                index   = self.get_index_from_coordinate(y_index, x_index)
+                current.append ( index)
+                if current == self.blacks :
+                    current = self.whites
+                else : current = self.blacks
+        
+    def make_features(self):
+        size = self.width * self.height
+        none_feature  = self.make_list(size,1)
+        black_feature = self.make_list(size,0)
+        white_feature = self.make_list(size,0)
+        features = []
+        features.append([none_feature, black_feature, white_feature])
+        for b,w in zip (self.blacks, self.whites) :
+            n_ = copy.deepcopy( features[-1][0])
+            b_ = copy.deepcopy( features[-1][1])
+            w_ = copy.deepcopy( features[-1][2])
+            b_[b] = 1
+            n_[b] = 0
+            features.append ( [n_,b_,w_])
+            
+            n_ = copy.deepcopy( features[-1][0])
+            b_ = copy.deepcopy( features[-1][1])
+            w_ = copy.deepcopy( features[-1][2])
+            w_[w] = 1
+            n_[w] = 0
+            features.append ( [n_,b_,w_])
+            
+        return features 
+
+    def make_input_targets_for_white_win(self):
+        inputTargets = [] #YixinSave.InputTargets()
+        features = self.make_features()
+        for i, white in zip ( range(1,features.__len__(),2 ) 
+                             ,self.whites ):
+            inputTarget = YixinSave.InputTarget()
+            inputTarget.input  = features[i]
+            inputTarget.target = self.make_target( white)
+            inputTargets.append( inputTarget )
+        return inputTargets
+    
+    def make_input_targets_for_black_win(self):
+        inputTargets = [] #YixinSave.InputTargets()
+        features = self.make_features()
+        for i, black in zip ( range(0,features.__len__(),2 ) 
+                             ,self.blacks ):
+            inputTarget = YixinSave.InputTarget()
+            inputTarget.input  = features[i]
+            inputTarget.target = self.make_target( black)
+            inputTargets.append( inputTarget )
+        return inputTargets
+    
+    ##########################################
+    
+    def get_index_from_coordinate(self, y, x):
+        return y * self.width + x
+    
+    def make_list(self, size, value):
+        l = []
+        for _ in range(size):
+            l.append (value)
+        return l
+
+    def make_target(self, one_hot_index):
+        l = self.make_list( self.width * self.height , 0)
+        l [ one_hot_index] = 1
+        return l
+    
+    class InputTarget:
+        def __init__(self):
+            self.input = []
+            self.target = []
---
+++ test/yixin/reader_test.py
@@ -0,0 +1,153 @@
+'''
+Created on 2017. 2. 1.
+
+@author: bi
+'''
+import unittest
+import os
+
+from yixin.reader import YixinSave
+
+class Test(unittest.TestCase):
+
+
+    def setUp(self):
+        self.mock = YixinSave()
+
+    def test_temp_file_write(self):
+        path = 'tmp.txt'
+        with open ( path, 'w') as f :
+            f.write( 'line1\n')
+            f.write( 'line2\n')
+        with open ( path ) as f :
+            lines = f.readlines()
+            self.assertEquals ( 'line1', lines[0][:-1])
+            self.assertEquals ( 'line2', lines[1][:-1])
+        os.remove(path)
+        
+    def test_read_width_num (self):
+        path = 'tmp.txt'
+        with open ( path, 'w') as f :
+            f.write( '3\n')
+            f.write( '5\n')
+            f.write( '2\n')
+        self.mock.read_yixin(path)
+        self.assertEquals ( 3, self.mock.width)
+        self.assertEquals ( 5, self.mock.height)
+        self.assertEquals ( 2, self.mock.num)
+        
+    def test_string_split(self):
+        s = '7 7'
+        splited = s.split(' ')
+        self.assertEquals ( '7', splited[0])
+        self.assertEquals ( '7', splited[1])
+        
+    def test_cordinate_to_index(self):
+        save = self.mock
+        save.width = 5 
+        self.assertEquals ( 3, save.get_index_from_coordinate(0, 3))
+        self.assertEquals ( 8, save.get_index_from_coordinate(1, 3))
+        save.width = 15 
+        # 15 * 7 = 105 , 105 + 7 = 112
+        self.assertEquals ( 112, save.get_index_from_coordinate(7,7))
+        self.assertEquals ( 111, save.get_index_from_coordinate(7,6))
+         
+    def test_read_first_black_and_white(self):
+        path = 'tmp.txt'
+        with open ( path, 'w') as f :
+            f.write( '10\n10\n2\n7 7\n8 8')
+        self.mock.read_yixin(path)
+        self.assertEqual([77], self.mock.blacks)
+        self.assertEqual([88], self.mock.whites)
+    
+    def test_last_element(self):
+        l = [ 1,2,3 ]
+        self.assertEqual(3, l[-1])
+        
+    def test_make_features (self):
+        save = YixinSave()
+        save.width = 3
+        save.height = 3
+        save.blacks = [0,1,2]
+        save.whites = [3,4,5]
+        features = save.make_features ()
+        self.assertEqual([1,1,1,1,1,1,1,1,1], features[0][0] ) # none
+        self.assertEqual([0,0,0,0,0,0,0,0,0], features[0][1] ) # black
+        self.assertEqual([0,0,0,0,0,0,0,0,0], features[0][2] ) # white
+        
+        self.assertEqual([0,1,1,1,1,1,1,1,1], features[1][0] ) # none
+        self.assertEqual([1,0,0,0,0,0,0,0,0], features[1][1] ) # black
+        self.assertEqual([0,0,0,0,0,0,0,0,0], features[1][2] ) # white
+        
+        self.assertEqual([0,1,1,0,1,1,1,1,1], features[2][0] ) # none
+        self.assertEqual([1,0,0,0,0,0,0,0,0], features[2][1] ) # black
+        self.assertEqual([0,0,0,1,0,0,0,0,0], features[2][2] ) # white
+        
+        self.assertEqual([0,0,1,0,1,1,1,1,1], features[3][0] ) # none
+        self.assertEqual([1,1,0,0,0,0,0,0,0], features[3][1] ) # black
+        self.assertEqual([0,0,0,1,0,0,0,0,0], features[3][2] ) # white
+        
+        self.assertEqual([0,0,1,0,0,1,1,1,1], features[4][0] ) # none
+        self.assertEqual([1,1,0,0,0,0,0,0,0], features[4][1] ) # black
+        self.assertEqual([0,0,0,1,1,0,0,0,0], features[4][2] ) # white
+        
+        self.assertEqual([0,0,0,0,0,1,1,1,1], features[5][0] ) # none
+        self.assertEqual([1,1,1,0,0,0,0,0,0], features[5][1] ) # black
+        self.assertEqual([0,0,0,1,1,0,0,0,0], features[5][2] ) # white
+        
+        self.assertEqual([0,0,0,0,0,0,1,1,1], features[6][0] ) # none
+        self.assertEqual([1,1,1,0,0,0,0,0,0], features[6][1] ) # black
+        self.assertEqual([0,0,0,1,1,1,0,0,0], features[6][2] ) # white
+        
+    def test_make_input_target_for_white_win(self):
+        save = YixinSave()
+        save.width = 3
+        save.height = 3
+        save.blacks = [0,1,2]
+        save.whites = [3,4,5]
+        input_and_targets  = save.make_input_targets_for_white_win()
+        input = input_and_targets[0].input
+        self.assertEqual([0,1,1,1,1,1,1,1,1], input[0] ) # none
+        self.assertEqual([1,0,0,0,0,0,0,0,0], input[1] ) # black
+        self.assertEqual([0,0,0,0,0,0,0,0,0], input[2] ) # white
+        self.assertEqual([0,0,0,1,0,0,0,0,0], input_and_targets[0].target)
+        
+        input = input_and_targets[1].input
+        self.assertEqual([0,0,1,0,1,1,1,1,1], input[0] ) # none
+        self.assertEqual([1,1,0,0,0,0,0,0,0], input[1] ) # black
+        self.assertEqual([0,0,0,1,0,0,0,0,0], input[2] ) # white
+        self.assertEqual([0,0,0,0,1,0,0,0,0], input_and_targets[1].target)
+    
+    def test_make_input_target_for_black_win(self):
+        save = self.mock
+        save.width = 3
+        save.height = 3
+        save.blacks = [0,1,2]
+        save.whites = [3,4,5]
+        input_and_targets  = save.make_input_targets_for_black_win()
+        input = input_and_targets[0].input
+        self.assertEqual([1,1,1,1,1,1,1,1,1], input[0] ) # none
+        self.assertEqual([0,0,0,0,0,0,0,0,0], input[1] ) # black
+        self.assertEqual([0,0,0,0,0,0,0,0,0], input[2] ) # white
+        self.assertEqual([1,0,0,0,0,0,0,0,0], input_and_targets[0].target)
+        
+        input = input_and_targets[1].input
+        self.assertEqual([0,1,1,0,1,1,1,1,1], input[0] ) # none
+        self.assertEqual([1,0,0,0,0,0,0,0,0], input[1] ) # black
+        self.assertEqual([0,0,0,1,0,0,0,0,0], input[2] ) # white
+        self.assertEqual([0,1,0,0,0,0,0,0,0], input_and_targets[1].target)
+    
+    
+    def test_read_white_win_on_many_files(self):
+        inputTargetSet = []
+        dir = 'yixin'
+        for f in os.listdir(dir) :
+            path = dir + os.sep + f
+            if f[0] == 'w' :
+                inputTargetSet.append( self.mock.read_white_win(path) )
+        print inputTargetSet
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.test_file_read']
+    unittest.main()
