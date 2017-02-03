04bfb72d9879059e0a10194a9e8d11eee17562fe
bi <a@a>
2017-02-03 17:31:35 +0900

    relu,  loss , limit accuracy .95

--- src/omok/omok_model.py
+++ src/omok/omok_model.py
@@ -17,8 +17,20 @@ class OmokModel(Model):
         self.feature_size = 3
         
     def create(self):
-        self.add_layer(self.width * self.width * self.feature_size)
-        self.add_layer(50)
+        self.add_layer(self.width * self.width * self.feature_size
+                       , act_func=tf.nn.relu, stddev=.02)
+        self.add_layer(50
+                       , act_func=tf.nn.relu, stddev=.02)
         self.add_layer(self.width * self.width,act_func=tf.nn.softmax )
+        
+    def init_loss(self):
+        self.loss = tf.reduce_sum( .5 *  tf.pow(self.target - self.output , 2) )
+    
+    def one_step(self, loop_index, sess, feed):
+        Model.one_step(self, loop_index, sess, feed)
+        if self.get_acc() > .95 :
+            self.is_stop = True
+            print 'skip acc > .95'
+    
     
     
